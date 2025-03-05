535c106812623c36bb14fdebcab34e9f747605e5
Saidev Dhal <connect.saidev@gmail.com>
2025-03-06 03:27:15 +0530

    add "cloudflare-turnstile" plugin

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@iflow/db";
+import { captcha } from "better-auth/plugins";
 
 const BaseDomain = process.env.NODE_ENV === "production" ? "https://api.plura.pro": "http://localhost:3001";
 
@@ -11,6 +12,13 @@ export const auth = betterAuth({
         provider: "postgresql",
     }),
     secret: process.env.BETTER_AUTH_SECRET,
+    plugins: [ 
+      captcha({ 
+          provider: "cloudflare-turnstile", // or "google-recaptcha" //
+          secretKey: process.env.TURNSTILE_SECRET_KEY!, 
+          endpoints: ["/sign-in"]
+      }), 
+  ], 
     socialProviders: {
         github: {
           clientId: process.env.GITHUB_CLIENT_ID!,
