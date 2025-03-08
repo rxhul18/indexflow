e7d197e18c60b4a176cc30d7d07a86178da8ebca
Saidev Dhal <connect.saidev@gmail.com>
2025-03-08 16:47:14 +0530

    switch package-manager to pnpm

--- apps/api/app/page.tsx
+++ apps/api/app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div>
+    <div className="bg-black text-white h-full w-full flex items-center justify-center font-semibold">
       It&apos;s fine to be broken sometimes.
     </div>
   );
