7ef70345aed35a556c485adc28f5c14e9b8b6f1e
rxhul18 <rahulshah0728@gmail.com>
2025-03-25 22:22:38 +0530

    Enhance zustand stores with TypeScript types and persistence

--- apps/web/lib/zustand.ts
+++ apps/web/lib/zustand.ts
@@ -1,34 +1,45 @@
+import { TagType, UserType } from '@iflow/types'
 import { create } from 'zustand'
+import { persist } from "zustand/middleware";
 
 type TagStore = {
-  tags: string[]
-  addTag: (tag: string) => void
-  removeTag: (tag: string) => void
-  setTags: (tags: string[]) => void
+  tags: TagType[]
+  addTag: (tag: TagType) => void
+  removeTag: (tagId: string) => void
+  setTags: (tags: TagType[]) => void
 }
 
 type UserStore = {
-  users: string[]
-  addUser: (user: string) => void
-  removeUser: (user: string) => void
-  setUsers: (users: string[]) => void
+  users: UserType[]
+  addUser: (user: UserType) => void
+  removeUser: (userId: string) => void
+  setUsers: (users: UserType[]) => void
 }
-const useTagsStore = create<TagStore>((set) => ({
-  tags: [],
-  addTag: (tag) =>
-    set((state) => ({ tags: [...state.tags, tag] })),
-  removeTag: (tag) =>
-    set((state) => ({ tags: state.tags.filter((t) => t !== tag) })),
-  setTags: (tags) => set({ tags }),
-}))
 
-const useUsersStore  = create<UserStore>((set) => ({
+const useTagsStore = create<TagStore>()(
+  persist((set)=>({
+    tags: [],
+    addTag: (tag)=> set((state)=>({tags:[...state.tags, tag]})),
+    removeTag: (tagId) => set((state)=>({tags:state.tags.filter((t)=> t.id !== tagId)})),
+    setTags: (tags) => set({tags})
+  }),
+  {
+    name: 'tags-storage'
+  }
+  )
+)
+
+const useUsersStore = create<UserStore>()(
+  persist((set)=>({
     users: [],
-    addUser: (user) =>
-      set((state) => ({ users: [...state.users, user] })),
-    removeUser: (user) =>
-      set((state) => ({ users: state.users.filter((t) => t !== user) })),
-    setUsers: (users) => set({ users }),
-  }))
+    addUser: (user)=> set((state)=>({users: [...state.users, user]})),
+    removeUser: (userId) => set((state)=>({users: state.users.filter((u)=> u.id !== userId)})),
+    setUsers: (users) => set({users})
+  }),
+  {
+    name: 'users-storage'
+  }
+  )
+)
 
-export {useTagsStore, useUsersStore};
+export { useTagsStore, useUsersStore }
