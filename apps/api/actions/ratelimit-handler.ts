5aef5559eb4251913f886e17daed6a20fde338e9
Saidev Dhal (Dev) <connect.saidev@gmail.com>
2025-03-27 12:14:47 +0530

    feat(api): fix ratelimitting for anon users

--- apps/api/actions/ratelimit-handler.ts
+++ apps/api/actions/ratelimit-handler.ts
@@ -12,12 +12,19 @@ export const rateLimitHandler = async (c: Context, next: Next) => {
   const rateLimit = new Ratelimit({
     redis: cache,
     limiter: Ratelimit.slidingWindow(limit, "1m"),
-    analytics: true, // store analytics data in redis db
+    analytics: true, 
   });
 
-  const { success } = await rateLimit.limit(
-    session?.session.ipAddress || session?.session.userId || "anonymous",
-  );
+  const key =
+  session?.session.ipAddress?.toString() ||
+  session?.session.userId?.toString() ||
+  c.req.header("CF-Connecting-IP") ||
+  c.req.header("X-Forwarded-For") ||
+  c.req.raw.headers.get("x-real-ip") ||
+  c.req.raw.headers.get("host") ||
+  "fallback-anon-key";
+
+  const { success } = await rateLimit.limit(key);
 
   if (!success) {
     return c.json({ message: "You hit the rate limit" }, 429);
